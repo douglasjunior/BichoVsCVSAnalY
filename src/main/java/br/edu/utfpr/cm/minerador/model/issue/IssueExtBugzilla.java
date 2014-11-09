a1de5c8f2555c9e3e2ef4768e3ecc2226d0d9ac1
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2014-11-09 10:48:40 -0200

    Fix imports

--- src/main/java/br/edu/utfpr/cm/minerador/model/issue/IssueExtBugzilla.java
+++ src/main/java/br/edu/utfpr/cm/minerador/model/issue/IssueExtBugzilla.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.utfpr.cm.minerador.model.issue;
 
 import java.io.Serializable;
@@ -14,12 +9,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
